1dc8211a9a8cae7efe865034fd1e5ada91ffa709
ashu02-git <shuhei.abe1107@gmail.com>
2021-05-26 15:34:38 +0900

    基本操作実装

---
+++ assets/scripts/app.js
@@ -0,0 +1,26 @@
+const $num1 = document.getElementById("num1");
+const $num2 = document.getElementById("num2");
+
+const $add = document.getElementById("btn-add");
+const $subtract = document.getElementById("btn-subtract");
+const $multiply = document.getElementById("btn-multiply");
+const $divide = document.getElementById("btn-divide");
+
+let calcResult = document.getElementById("calc-result");
+
+
+$add.onclick = function () {
+    calcResult.innerText = parseInt(num1.value) + parseInt($num2.value);
+};
+
+$subtract.onclick = function () {
+    calcResult.innerText =  parseInt(num1.value) - parseInt($num2.value);
+};
+
+$multiply.onclick = function () {
+    calcResult.innerText = resultText = parseInt(num1.value) * parseInt($num2.value);
+};
+
+$divide.onclick = function () {
+    calcResult.innerText = parseInt(num1.value) / parseInt($num2.value);;
+};
